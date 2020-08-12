3b9ded3046ad4a0e83b80b079d9a6d4c3d35f4d6
Kade <sugiantara.igk@gmail.com>
2020-08-12 22:50:02 +0700

    add alert component, alert redux action and aler redux reducer

---
+++ src/components/Alert.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Alert = () => {
+  const alerts = useSelector((state) => state.alert);
+  console.log(alerts);
+  return (
+    <div className="container fixed-bottom">
+      {alerts !== null &&
+        alerts.length > 0 &&
+        alerts.map((alert) => {
+          return (
+            <div
+              key={alert.id}
+              className={`alert alert-${alert.type} text-center`}
+            >
+              {alert.msg}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Alert;
