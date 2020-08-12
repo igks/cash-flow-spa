a92e7fd3a43fc6db2ac7620aa561a49bad37ed34
Kade <sugiantara.igk@gmail.com>
2020-08-12 21:09:41 +0700

    add auto token header set for axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./displays/Login";
+import SignUp from "./displays/SignUp";
+import Home from "./displays/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
