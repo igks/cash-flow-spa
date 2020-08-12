c125a2438a8aa5eca5b200d1d28b460de57cac48
Kade <sugiantara.igk@gmail.com>
2020-08-12 22:48:30 +0700

    add redux to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import reduxStore from "./redux/store";
+import setAuthToken from "./services/setAuthToken";
 import Login from "./displays/Login";
 import SignUp from "./displays/SignUp";
 import Home from "./displays/Home";
+import Alert from "./components/Alert";
 
-function App() {
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
   return (
     <div className="container">
-      <Router>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/login" component={Login} />
-        <Switch>
-          <Route path="/signup" component={SignUp} />
-          <Route path="/home" component={Home} />
-        </Switch>
-      </Router>
+      <Provider store={reduxStore}>
+        <Router>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </Router>
+        <Alert />
+      </Provider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const reduxStore = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools()
+);
+
+export default reduxStore;
